72532f5f5218b1e3de7b4a0228bba8b015c6b095
Mario Hernandez <yo@mariofix.com>
2019-05-19 18:42:26 -0400

    Config data

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 class database:
-    hostname = '200.27.36.114'
+    hostname = '127.0.0.1'
     port = 3306
-    username = 'zvn'
-    password = 'zvn.r0t5y3'
-    database = 'zvn_app_Dev'
+    username = 'root'
+    password = 'root'
+    database = 'database'
     dbdriver = 'mysql'
     dbprefix = ''
     charset = 'utf8'
